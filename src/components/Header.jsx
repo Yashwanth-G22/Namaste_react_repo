87e7ad57611f05820d7a10e6dbaca0bfdf785d47
gownipalli.yeswanth <gownipalli.yeswanth@agilepoint.com>
2024-02-28 09:57:16 +0530

    added constants ts file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import { LOGO } from "./constants"
+
 export const Header = () => {
     return (
         <div className="header">
             <div className="logo-container">
-                <img src="" alt="" />
+                <img src={LOGO} alt="" />
             </div>
             <div className="nav-items">
             <ul>
---
+++ src/components/constants.ts
@@ -0,0 +1,1 @@
+export const LOGO = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKZ18W-E-GH3L_Xcn3T42WkxFRMXxyn9cmQQ&usqp=CAU';
