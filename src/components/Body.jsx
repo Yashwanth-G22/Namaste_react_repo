75fc58c829e459a30e317371a0ee88b9b55767a0
gownipalli.yeswanth <gownipalli.yeswanth@agilepoint.com>
2024-03-01 17:57:45 +0530

    chore: going to add the search bar in soon

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { items } from './items';
 import { RestaurantCard } from './Rescard';
 import { Footer } from './footer/footer';
 import { contactNo, webLink } from './constants';
 
 export const Body = () => {
-  const [newFilterItems, setNewFilterItems] = useState();
+  const [newFilterItems, setNewFilterItems] = useState(items);
+  const searchText = useRef();
+  const handleSearch = () => {
+    console.log(searchText.current.value);
+  }
+  console.log(searchText)
   const filterItems = () => {
     setNewFilterItems(items.filter(res => res.rating > 3.5))
   };
   return (
     <div>
       <div className="search-bar">
-        <button onClick={filterItems}>Filter res</button>
+        <div className='search'>
+          <input type="search" className='search-input' defaultValue='fruits' ref={searchText}/>
+          <button className='search-btn' onClick={handleSearch}>Search</button>
+        </div>
       </div>
       <div className="res-container">
         <RestaurantCard items={newFilterItems ? newFilterItems : items} />
