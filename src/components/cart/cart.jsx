288654c41831bf00a8cf0eb57d4703ffe9f417c3
gownipalli.yeswanth <gownipalli.yeswanth@agilepoint.com>
2024-03-06 18:35:23 +0530

    chore added custom hooks

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,9 +1,14 @@
+import { useRestrounentMenu } from "../../utils/useRestrounentMenu";
 
 export const Cart = () => {
+
+    const { resInfo } = useRestrounentMenu();
+
     return (
         <div>
             <h1>Cart</h1>
             <h3>The things are shown, where you going buy soon</h3>
+            <h1>{resInfo}</h1>
         </div>
     )
 };
