f66ae9500f9045e3ae9b205b5ae08c4e160bac7f
gownipalli.yeswanth <gownipalli.yeswanth@agilepoint.com>
2024-03-13 17:11:19 +0530

    chore: added higher order component

--- src/components/Rescard.jsx
+++ src/components/Rescard.jsx
@@ -1,15 +1,14 @@
 export const RestaurantCard = (props) => {
-    const {items} = props;
-    const renderFD =() => items.map(({resName,foodName, rating, delivery, image}) => {
-        return (
+    const { resData } = props;
+    const {delivery, foodName, resName, image, rating} = resData;
+
+    return (
         <div className="card" key={resName}>
-        <img  src={image} alt="" />
-        <h3>{resName}</h3>
-        <h4>{foodName}</h4>
-        <h4>{rating} star</h4>
-        <h4>{delivery} mins</h4>
-    </div>)
-})
-    return  (renderFD());
-    
+            <img src={image} alt="" />
+            <h3>{resName}</h3>
+            <h4>{foodName}</h4>
+            <h4>{rating} star</h4>
+            <h4>{delivery} mins</h4>
+        </div>)
+
 }
